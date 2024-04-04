fb489193c2e01704a7fc9f1c18163186310ce956
11aishwarya <raiaishwarya003@gmail.com>
2024-04-04 15:27:10 +0530

    String Subset Question using Recursion

---
+++ src/Recursion/Substr.java
@@ -0,0 +1,63 @@
+package Recursion;
+
+public class Substr {
+    public static void main(String[] args) {
+//       String str = "bcaadef";
+//       skip("",str);
+        System.out.println(skip2("aabcdesaaa"));
+        System.out.println(skipapple("abcappleef"));
+        System.out.println(skipappnotapple("appleappcdesf"));
+    }
+
+    public static void skip(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            skip(p,up.substring(1));
+        }
+        else{
+            skip(p+ch,up.substring(1));
+        }
+    }
+
+    public static String skip2(String up) {
+        if (up.isEmpty()) {
+            //System.out.println(p);
+            return "";
+        }
+        char ch = up.charAt(0);
+        if (ch == 'a') {
+            return skip2(up.substring(1));
+        } else {
+            return ch + skip2(up.substring(1));
+        }
+    }
+
+    public static String skipapple(String up) {
+        if (up.isEmpty()) {
+            //System.out.println(p);
+            return "";
+        }
+       // char ch = up.charAt(0);
+        if (up.startsWith("apple")) {
+            return skipapple(up.substring(5));
+        } else {
+            return up.charAt(0) + skipapple(up.substring(1));
+        }
+    }
+    public static String skipappnotapple(String up) {
+        if (up.isEmpty()) {
+            //System.out.println(p);
+            return "";
+        }
+        // char ch = up.charAt(0);
+        if (up.startsWith("app") && !up.startsWith("apple")) {
+            return skipappnotapple(up.substring(3));
+        } else {
+            return up.charAt(0) + skipappnotapple(up.substring(1));
+        }
+    }
+}
