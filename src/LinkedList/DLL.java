a427300e2649adec02f368811208a958ce010077
11aishwarya <raiaishwarya003@gmail.com>
2024-07-08 12:55:56 +0530

    Doubly LL

--- src/LinkedList/DLL.java
+++ src/LinkedList/DLL.java
@@ -1,4 +1,89 @@
 package LinkedList;
 
 public class DLL {
+
+    private class Node{
+        int val;
+        Node next;
+        Node prev;
+
+        public Node(int val){
+            this.val = val;
+        }
+        public Node(int val,Node next,Node prev){
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+    Node head;
+    public void insertF(int val){
+        Node node = new Node(val);
+        node.next = head;
+        node.prev = null;
+        if(head != null){
+            head.prev = node;
+        }
+        head = node;
+    }
+
+    public void Display(){
+        Node node = head;
+        Node last = null;
+        while(node != null){
+            System.out.print(node.val + "->");
+            last = node;
+            node = node.next;
+        }
+        System.out.println();
+
+        System.out.println("print in reverse");
+        while(last != null){
+            System.out.print(last.val + "->");
+            last = last.prev;
+        }
+        System.out.print("Start");
+    }
+    public void insertLast(int val){
+        Node node = new Node(val);
+        Node last = head;
+        node.next = null;
+        if(head == null){
+            node.prev = null;
+            head = node;
+            return;
+        }
+        while(last.next != null){
+            last = last.next;
+        }
+        last.next = node;
+        node.prev = last;
+
+    }
+    public Node Find(int  val){
+        Node node = head;
+        while(node != null){
+            if(node.val == val){
+                return node;
+            }
+            node = node.next;
+        }
+        return node;
+    }
+
+    public void insertParticulatvalueAfter(int after , int val){
+        Node p = Find(after);
+
+        if(p != null){
+            System.out.println("Does not exit");
+            return;
+        }
+        Node node = new Node(val);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+        if(node.next != null){
+            node.next.prev = node;
+        }
+    }
 }
--- src/LinkedList/main.java
+++ src/LinkedList/main.java
@@ -4,18 +4,30 @@ public class main {
     public static void main(String[] args) {
 
 
-        ll list = new ll();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.inserLast(5);
-        list.insertFirst(1);
-        list.insertAnyIdx(4,3);
-        list.deleteLast();
-        list.deleteParticularIdx(2);
-        //list.get(2);
+//        ll list = new ll();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.inserLast(5);
+//        list.insertFirst(1);
+//        list.insertAnyIdx(4,3);
+//        list.deleteLast();
+//        list.deleteParticularIdx(2);
+//        //list.get(2);
+//
+//        list.display();
+//        list.get(2);
 
-        list.display();
-        list.get(2);
+
+        DLL list = new DLL();
+        list.insertF(3);
+        list.insertF(2);
+        list.insertF(4);
+        list.insertF(7);
+        list.insertLast(8);
+        list.insertLast(6);
+        list.insertParticulatvalueAfter(7,65);
+        list.Display();
+
     }
 
 }
